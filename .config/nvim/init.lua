fee3ceb839f66daabb9f12c59d786ab462a44f41
Tristan <tristan@example.com>
2026-03-09 16:30:08 +1100

    updated init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,26 +5,31 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.relativenumber = true
 
+-- vim.pack.add docs: https://neovim.io/doc/user/pack/#vim.pack.add()
 vim.pack.add({
+	"https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+
+	"https://github.com/morhetz/gruvbox",
+
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
+
 	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/nvim-telescope/telescope.nvim",
 
 	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/catppuccin/nvim",
 	"https://github.com/karb94/neoscroll.nvim",
 
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-tree/nvim-tree.lua",
 })
 
--- Add nvim-lspconfig path to
--- package.path = package.path .. ";/home/tristan/.config/nvim/lsp/?.lua"
-
--- These are probably settings you'll want for autocomplete.
--- For info see: https://neovim.io/doc/user/options/#'completeopt'
+-- Sensible settings for autocomplete.
+-- See here for more info: https://neovim.io/doc/user/options/#'completeopt'
 vim.opt.completeopt = { "menuone", "noselect", "fuzzy" }
 
--- Config source: https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ty.lua
+-- ty lsp
+-- https://docs.astral.sh/ty/editors/#vs-code
 ty_conf = {
 	cmd = { "ty", "server" },
 	filetypes = { "python" },
@@ -39,24 +44,34 @@ ty_conf = {
 				return { abbr = item.label:gsub("%b()", "") }
 			end,
 		})
+
+		-- Triggers autocompletion in insert mode with Ctrl+Space
 		vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+
+		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "trigger autocompletion" })
+
+		-- Jumps to code definition when pressing gd in normal mode
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to code definition" })
 	end,
 }
-
 vim.lsp.config("ty", ty_conf)
-
 vim.lsp.enable("ty")
 
--- vim.cmd.colorscheme("catppuccin")
-vim.cmd.colorscheme("habamax")
+-- gruvbox
+vim.cmd.colorscheme("gruvbox")
 
--- Need to call this for neoscroll to work.
+-- neoscroll
 require("neoscroll").setup()
 
--- Only thing we have to do after installing nvim tree
+-- nvim-tree
 require("nvim-tree").setup()
 
+-- telescope
 local builtin = require("telescope.builtin")
-
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "Lists LSP references for word under the cursor" })
+-- You must maually buld fzf after vim.pack downloads it:
+-- 1) Go to: ~/.local/share/nvim/site/pack/core/opt/telescope-fzf-native.nvim
+-- 2) Run `make`
+require("telescope").load_extension("fzf")
